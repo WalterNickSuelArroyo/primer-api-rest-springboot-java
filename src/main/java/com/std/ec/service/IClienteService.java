af03008f601fce608f8b4602a85b7babdf753636
WalterNickSuelArroyo <nicodehero21@outlook.com>
2023-12-19 10:29:59 -0500

    Subida de proyecto en springboot

---
+++ src/main/java/com/std/ec/service/IClienteService.java
@@ -0,0 +1,14 @@
+package com.std.ec.service;
+
+import com.std.ec.model.dto.ClienteDto;
+import com.std.ec.model.entity.Cliente;
+
+import java.util.List;
+
+public interface IClienteService {
+    List<Cliente> listAll();
+    Cliente save(ClienteDto cliente);
+    Cliente findById(Integer id);
+    void delete(Cliente cliente);
+    boolean existsById(Integer id);
+}
